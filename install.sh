56e66dcb27247fc030f488648f21da76951c3338
GospelCoding33 <130040013+GospelHack33@users.noreply.github.com>
2023-06-08 09:55:54 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+# install nmap package
+echo "[+] Installing MapSploit Package... " 
+echo
+apt install nmap
+apt install python
+echo
+echo "[+] Successfully Installed Required Package "
+echo
+echo "[+] Type (python mapsploit.py) To Start MapSploit... "
+echo 
+
